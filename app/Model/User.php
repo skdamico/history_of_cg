d715779add7e6edd5f5f14db0025e98130983227
Stefano DAmico <skdamico@gmail.com>
2012-02-05 16:35:28 -0500

    Updated db import file. Changed ACL to group-only permissions

--- app/Model/User.php
+++ app/Model/User.php
@@ -7,8 +7,6 @@ class User extends AppModel {
 
   public $belongsTo = array('Group');
 
-  public $actsAs = array('Acl' => array('type' => 'requester'));
-
   public $validate = array(
     'first_name' => array(
       'notEmpty' => array(
@@ -50,24 +48,6 @@ class User extends AppModel {
 
   public function bindNode($user) {
     return array('model' => 'Group', 'foreign_key' => $user['User']['group_id']);
-  }
-
-  public function parentNode() {
-    if (!$this->id && empty($this->data)) {
-      return null;
-    }
-    if (isset($this->data['User']['group_id'])) {
-      $groupId = $this->data['User']['group_id'];
-    }
-    else {
-      $groupId = $this->field('group_id');
-    }
-    if (!$groupId) {
-      return null;
-    }
-    else {
-      return array('Group' => array('id' => $groupId));
-    }
   }
 
   // This method gets called when validating the virtual password_confirm field
--- setup/setup_db_schema.sql
+++ setup/setup_db_schema.sql
@@ -7,7 +7,7 @@
 #
 # Host: localhost (MySQL 5.5.20)
 # Database: historyofcg
-# Generation Time: 2012-01-31 22:04:53 +0000
+# Generation Time: 2012-02-05 21:33:24 +0000
 # ************************************************************
 
 
@@ -18,6 +18,118 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table acos
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `acos`;
+
+CREATE TABLE `acos` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `parent_id` int(10) DEFAULT NULL,
+  `model` varchar(255) DEFAULT '',
+  `foreign_key` int(10) unsigned DEFAULT NULL,
+  `alias` varchar(255) DEFAULT '',
+  `lft` int(10) DEFAULT NULL,
+  `rght` int(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+LOCK TABLES `acos` WRITE;
+/*!40000 ALTER TABLE `acos` DISABLE KEYS */;
+
+INSERT INTO `acos` (`id`, `parent_id`, `model`, `foreign_key`, `alias`, `lft`, `rght`)
+VALUES
+	(1,NULL,'',NULL,'controllers',1,54),
+	(2,1,NULL,NULL,'About',2,3),
+	(3,1,NULL,NULL,'Entries',4,13),
+	(4,1,NULL,NULL,'Groups',14,25),
+	(5,4,NULL,NULL,'index',15,16),
+	(6,4,NULL,NULL,'view',17,18),
+	(7,4,NULL,NULL,'add',19,20),
+	(8,4,NULL,NULL,'edit',21,22),
+	(9,4,NULL,NULL,'delete',23,24),
+	(10,1,NULL,NULL,'Home',26,29),
+	(11,10,NULL,NULL,'index',27,28),
+	(12,1,NULL,NULL,'Sessions',30,35),
+	(13,12,NULL,NULL,'create',31,32),
+	(14,12,NULL,NULL,'destroy',33,34),
+	(15,1,NULL,NULL,'Users',36,51),
+	(16,15,NULL,NULL,'signup',37,38),
+	(17,15,NULL,NULL,'index',39,40),
+	(18,15,NULL,NULL,'view',41,42),
+	(19,15,NULL,NULL,'add',43,44),
+	(20,15,NULL,NULL,'edit',45,46),
+	(21,15,NULL,NULL,'delete',47,48),
+	(22,1,NULL,NULL,'AclExtras',52,53),
+	(23,3,NULL,NULL,'index',5,6),
+	(24,3,NULL,NULL,'view',7,8),
+	(25,3,NULL,NULL,'add',9,10),
+	(26,3,NULL,NULL,'edit',11,12),
+	(27,15,NULL,NULL,'initDB',49,50);
+
+/*!40000 ALTER TABLE `acos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table aros
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `aros`;
+
+CREATE TABLE `aros` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `parent_id` int(10) DEFAULT NULL,
+  `model` varchar(255) DEFAULT '',
+  `foreign_key` int(10) unsigned DEFAULT NULL,
+  `alias` varchar(255) DEFAULT '',
+  `lft` int(10) DEFAULT NULL,
+  `rght` int(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+LOCK TABLES `aros` WRITE;
+/*!40000 ALTER TABLE `aros` DISABLE KEYS */;
+
+INSERT INTO `aros` (`id`, `parent_id`, `model`, `foreign_key`, `alias`, `lft`, `rght`)
+VALUES
+	(1,NULL,'Group',1,'',1,2),
+	(2,NULL,'Group',2,'',3,4);
+
+/*!40000 ALTER TABLE `aros` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+# Dump of table aros_acos
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `aros_acos`;
+
+CREATE TABLE `aros_acos` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `aro_id` int(10) unsigned NOT NULL,
+  `aco_id` int(10) unsigned NOT NULL,
+  `_create` char(2) NOT NULL DEFAULT '0',
+  `_read` char(2) NOT NULL DEFAULT '0',
+  `_update` char(2) NOT NULL DEFAULT '0',
+  `_delete` char(2) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+LOCK TABLES `aros_acos` WRITE;
+/*!40000 ALTER TABLE `aros_acos` DISABLE KEYS */;
+
+INSERT INTO `aros_acos` (`id`, `aro_id`, `aco_id`, `_create`, `_read`, `_update`, `_delete`)
+VALUES
+	(1,1,1,'1','1','1','1'),
+	(2,2,1,'-1','-1','-1','-1'),
+	(3,2,25,'1','1','1','1'),
+	(4,2,26,'1','1','1','1'),
+	(5,2,15,'-1','-1','-1','-1');
+
+/*!40000 ALTER TABLE `aros_acos` ENABLE KEYS */;
+UNLOCK TABLES;
 
 
 # Dump of table categories
@@ -95,6 +207,31 @@ CREATE TABLE `entries_tags` (
   CONSTRAINT `entries_tags_ibfk_2` FOREIGN KEY (`tag_id`) REFERENCES `tags` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+
+
+# Dump of table groups
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `groups`;
+
+CREATE TABLE `groups` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL DEFAULT '',
+  `created` datetime DEFAULT NULL,
+  `modified` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+LOCK TABLES `groups` WRITE;
+/*!40000 ALTER TABLE `groups` DISABLE KEYS */;
+
+INSERT INTO `groups` (`id`, `name`, `created`, `modified`)
+VALUES
+	(1,'admins','2012-02-03 16:05:42','2012-02-03 16:06:05'),
+	(2,'users','2012-02-03 16:06:11','2012-02-03 16:06:11');
+
+/*!40000 ALTER TABLE `groups` ENABLE KEYS */;
+UNLOCK TABLES;
 
 
 # Dump of table locations
@@ -175,6 +312,7 @@ CREATE TABLE `users` (
   `last_name` varchar(30) NOT NULL DEFAULT '',
   `email` varchar(200) NOT NULL DEFAULT '',
   `password` varchar(50) NOT NULL DEFAULT '',
+  `group_id` int(11) unsigned NOT NULL DEFAULT '2',
   `created` datetime DEFAULT NULL,
   `modified` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
