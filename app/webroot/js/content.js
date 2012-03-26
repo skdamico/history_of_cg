952027f80051fdaa4968e2488af3cb23918bed27
Stefano DAmico <skdamico@gmail.com>
2012-03-26 17:38:21 -0400

    remove description limit

--- app/webroot/js/content.js
+++ app/webroot/js/content.js
@@ -93,12 +93,6 @@ $(function() {
             return false;
         });
 
-        // fix description tile character limit
-        var $description = $(".mosaicContainer .dynamic");
-        var $description_text = $description.html();
-        if($description_text.length > 300) {
-            $description.html($description_text.substring(0,300) + ' ...');
-        }
     }
 
 
