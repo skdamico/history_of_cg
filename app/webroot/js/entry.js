c3f60235c48f14851b3b6f6d601134c94a050c12
Connie Chan <hello.cchan@gmail.com>
2012-04-16 19:34:12 -0400

    1

--- app/webroot/js/entry.js
+++ app/webroot/js/entry.js
@@ -240,12 +240,14 @@ $(function() {
                 $('.basics .source-url').fadeIn();
                 $('.basics .label-entry-date').css('margin-top','280px');
                 $('li.label-entry-homepage').css("top","273px");
+                alert('yes');
             }
             else {
                 $('.basics .label-entry-date').css('margin-top','175px');
+                $('li.label-entry-homepage').css("top","170px"); 
                 $('.basics .source-title').hide();
                 $('.basics .source-url').hide();
-                $('li.label-entry-homepage').attr("style","top:170px"); 
+                alert('no');
             }
         }
 
