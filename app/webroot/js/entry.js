4f5c9db0be02733e47253d3e5fe267a571b92bf7
Connie Chan <hello.cchan@gmail.com>
2012-04-16 19:30:45 -0400

    w.t.f.

--- app/webroot/js/entry.js
+++ app/webroot/js/entry.js
@@ -246,7 +246,7 @@ $(function() {
                 $('.basics .label-entry-date').css('margin-top','175px');
                 $('.basics .source-title').hide();
                 $('.basics .source-url').hide();
-                $('li.label-entry-homepage').attr("style","top:170px!important"); 
+                $('li.label-entry-homepage').attr("style","top:170px"); 
             }
         }
 
