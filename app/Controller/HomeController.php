348774de49226a6b34da9286a3e352ff61472ca5
kmontag42 <gordon117@gmail.com>
2012-11-13 15:20:51 -0500

    trying controller + view method

--- app/Controller/HomeController.php
+++ app/Controller/HomeController.php
@@ -12,7 +12,7 @@ class HomeController extends AppController {
 
     public function index() {
         $this->layout = 'home';
-        $this->set('entries', $this->Entry->find('list', array('order' => 'Entry.created')));
+        $this->set('entries', $this->Entry->find('list', array('order' => 'Entry.created', 'contain' => array('Entry'))));
         $this->set('stories', $this->EntryStory->find('all', array(
                                              'contain' => array(
                                                  'Story' => array(
