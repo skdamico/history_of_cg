515f74ce5ea47003b67e96ef98d7e08f8ec3be02
kmontag42 <gordon117@gmail.com>
2012-11-13 13:57:01 -0500

    trying controller + view method

--- app/Controller/HomeController.php
+++ app/Controller/HomeController.php
@@ -14,9 +14,6 @@ class HomeController extends AppController {
         $this->layout = 'home';
         $this->set('allEntries', $this->Entry->find('list', array('order' => 'Entry.created')));
         $this->set('allStories', $this->EntryStory->find('all', array(
-                                             'conditions' => array(
-                                                 'EntryStory.entry_id' => $entry['Entry']['id']
-                                             ),
                                              'contain' => array(
                                                  'Story' => array(
                                                      'conditions' => array('Story.published' => 1),
