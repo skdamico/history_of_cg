6d0de91f007c8e2ea8ede24072e8cb84d40d0923
kmontag42 <gordon117@gmail.com>
2012-11-13 15:30:11 -0500

    trying controller + view method

--- app/Model/EntryStory.php
+++ app/Model/EntryStory.php
@@ -2,6 +2,8 @@
 
 class EntryStory extends AppModel {
   public $name = 'EntryStory';
+  public $thumbsUp = 0;
+  public $thumbsDown = 0;
   public $useTable = 'entries_stories';
   public $actsAs = array('Containable');
 
