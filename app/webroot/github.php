ae5c711d3116dd89f3fc47350240e9a823812e01
root <root@kittykat.historyofcg.com>
2012-11-29 17:41:20 -0500

    added github script to git

---
+++ app/webroot/github.php
@@ -0,0 +1,9 @@
+<?php
+
+// Use in the "Post-Receive URLs" section of your GitHub repo.
+
+if ( $_POST['payload'] ) {
+  shell_exec( 'cd /var/www/staging.historyofcg.com/historyofcg/ && git reset --hard HEAD && git pull origin master' );
+}
+
+?>
